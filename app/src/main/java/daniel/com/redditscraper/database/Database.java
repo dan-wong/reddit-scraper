c5d6ca1a7a1b4e0f11c574bcf63ab80bd997cee7
dan-wong <dan_wong@outlook.com>
2018-04-21 17:01:46 +1200

    Added Database class

---
+++ app/src/main/java/daniel/com/redditscraper/database/Database.java
@@ -0,0 +1,82 @@
+package daniel.com.redditscraper.database;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import daniel.com.redditscraper.Image;
+import daniel.com.redditscraper.async.redditscraper.RedditScraperAsyncTask;
+import daniel.com.redditscraper.async.redditscraper.RedditScraperCallback;
+
+//Singleton
+public class Database implements RedditScraperCallback {
+    private static Database instance;
+
+    private DatabaseCallback listener;
+
+    private Map<String, CachedImages> cache;
+    private String currentSubredditSearch;
+
+    private Database() {
+        cache = new HashMap<>();
+    }
+
+    public static Database getInstance() {
+        if (instance == null) {
+            return new Database();
+        } else {
+            return instance;
+        }
+    }
+
+    public void addListener(DatabaseCallback listener) {
+        this.listener = listener;
+    }
+
+    public void getImage(String subreddit) {
+        CachedImages cachedImages = cache.get(subreddit);
+
+        //Either first time we poll for this subreddit, or no images left
+        if (cachedImages == null || cachedImages.images.isEmpty()) {
+            //Get new images
+            currentSubredditSearch = subreddit;
+
+            String lastImageId = "";
+            if (cachedImages != null) {
+                lastImageId = cachedImages.getLastImageId();
+            }
+
+            new RedditScraperAsyncTask(this, subreddit, lastImageId).execute();
+        } else {
+            listener.imageReturned(cachedImages.images.remove(0));
+        }
+    }
+
+    private boolean checkIfImage(String url) {
+        return url.contains("jpeg") || url.contains("jpg") || url.contains("png");
+    }
+
+    @Override
+    public void images(List<Image> images) {
+        Iterator<Image> imageIterator = images.iterator();
+        while (imageIterator.hasNext()) {
+            if (!checkIfImage(imageIterator.next().url)) {
+                imageIterator.remove();
+            }
+        }
+
+        if (images.size() == 0) {
+            listener.error("Subreddit has no images :(");
+            return;
+        }
+
+        cache.put(currentSubredditSearch, new CachedImages(images));
+        getImage(currentSubredditSearch);
+    }
+
+    @Override
+    public void error(String message) {
+        listener.error(message);
+    }
+}
